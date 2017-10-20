2305fb7d05f218a93fb166b2869aaa87a39eae03
kairolamarca <lamarca.kairo@gmail.com>
2017-10-19 23:35:46 -0200

    websocket
    validando quando o cliente conectou e quando ele desconectou

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ let server = app.listen(80, () => {
     console.log('Servidor online na porta 80');
 })
 
-require('socket.io').listen(server);
+io = require('socket.io').listen(server);
+
+//criar a conexão por websocket
+io.on('connection', (socket) => {//evento pré definido no socket.io, executa quando há uma conexão do lado do cliente
+    console.log('usuario conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou');
+    });
+
+})
