d60b0fb00b50abecb42bbd26f73dff3048e39ba0
kairolamarca <lamarca.kairo@gmail.com>
2017-10-19 23:21:56 -0200

    integrando socket.io
    integrando socket.io no app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let app = require('./config/server');
 
 //parametrizar a porta de escuta
-app.listen(80, () => {
+let server = app.listen(80, () => {
     console.log('Servidor online na porta 80');
 })
+
+require('socket.io').listen(server);
